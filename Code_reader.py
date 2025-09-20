f24dce894876db57616b6fc363b96c403d4bc627
CelineLucas <cecelucas@aol.com>
2025-09-20 14:42:41 +0200

    tutorial is finished

--- Code_reader.py
+++ Code_reader.py
@@ -1,16 +1,29 @@
+from time import sleep
 import cv2 # read image/camera/video input
 from pyzbar.pyzbar import decode
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)   # 3 - Width
 cap.set(4, 480)   # 4 - Height
+used_codes = []
+
 camera = True
 while camera == True:
     succes, frame = cap.read()
 
     for code in decode(frame):
-        print(code.type)
-        print(code.data.decode('utf8'))
+        content = code.data.decode('utf8')
+        if content not in used_codes:
+            print('Approuved. You can enter !')
+            print(content)
+            used_codes.append(content)
+            sleep(5)
+        elif content is used_codes:
+            print('Sorry, this code has been already used !')
+            sleep(5)
+        else:
+            pass
+
 
     cv2.imshow('Testing-code-scan', frame)
     cv2.waitKey(1)
